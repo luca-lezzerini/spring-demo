d0c6d1f4b889934a8e0ec3c3b74c9b7c3de1d02d
luca-lezzerini <47611629+luca-lezzerini@users.noreply.github.com>
2021-02-26 15:45:53 +0100

    Aggiunto progetto server Spring

---
+++ src/test/java/it/iad/primo/PrimoApplicationTests.java
@@ -0,0 +1,13 @@
+package it.iad.primo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
